e857079a31c8a4e6da65551c51b78df2f1d600b7
Ahmed Aldahshoury <ahmed.aldahshoury@gmail.com>
2025-12-26 14:25:21 +0100

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,5 @@
 import { defineConfig } from "astro/config";
-import sitemap from "@astrojs/sitemap";
 
 export default defineConfig({
   site: "https://t7tfos.com",
-  integrations: [sitemap()],
 });
